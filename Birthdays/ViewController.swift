3a07390d565849f99c28d1a7ca36bfeb8ec566d4
dasdom <dom@dasdom.de>
2015-07-24 17:58:52 +0200

    Initial Commit

---
+++ Birthdays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Birthdays
+//
+//  Created by dasdom on 24.07.15.
+//  Copyright © 2015 dasdom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
