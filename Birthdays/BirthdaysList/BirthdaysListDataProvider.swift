78cb81bfc4b5ff7c2d63aa7030e20370c078d980
Jess <jessachandler@gmail.com>
2017-06-23 08:48:25 +0900

    update to Swift 3.1
    Update code to work with Swift 3.1

--- Birthdays/BirthdaysList/BirthdaysListDataProvider.swift
+++ Birthdays/BirthdaysList/BirthdaysListDataProvider.swift
@@ -10,61 +10,61 @@ import UIKit
 
 class BirthdaysListDataProvider: NSObject, UITableViewDataSource {
 
-  private let cellIdentifer = "Cell"
-  private var birthdays = [Birthday]()
-  private let gregorian = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
-  var todayComponents: NSDateComponents?
-  var today: NSDate? {
-    didSet {
-      if let today = today {
-        todayComponents = gregorian?.components([.Month, .Day, .Year], fromDate: today)
-      }
+    private let cellIdentifer = "Cell"
+    var birthdays = [Birthday]()
+    private let gregorian = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)
+    var todayComponents: NSDateComponents?
+    var today: NSDate? {
+        didSet {
+            if let today = today {
+                todayComponents = gregorian?.components([.month, .day, .year], from: today as Date) as NSDateComponents?
+            }
+        }
     }
-  }
-  
-  func registerCellsForTableView(tableView: UITableView) {
-    tableView.registerClass(BirthdayCell.self, forCellReuseIdentifier: cellIdentifer)
-  }
-  
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return birthdays.count
-  }
-  
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifer, forIndexPath: indexPath) as! BirthdayCell
-    
-    let birthday = birthdays[indexPath.row]
-    cell.updateWithItem(birthday, progress: progressUntilBirthday(birthday))
-    
-    return cell
-  }
-  
-  func progressUntilBirthday(birthday: Birthday) -> Float {
-    
-    guard let todayComponents = todayComponents else { return 0.0 }
 
-    let calculationComponents = birthday.birthday.copy() as! NSDateComponents
-    calculationComponents.year = todayComponents.year
-    
-    if calculationComponents.month < todayComponents.month ||
-      (calculationComponents.month == todayComponents.month &&
-        calculationComponents.day < todayComponents.day) {
-      
-      calculationComponents.year += 1 // Swift 3 compliant ...
+    func registerCellsForTableView(tableView: UITableView) {
+        tableView.register(BirthdayCell.self, forCellReuseIdentifier: cellIdentifer)
     }
-    
-    let components = gregorian?.components([.Day],
-                                           fromDateComponents: todayComponents,
-                                           toDateComponents: calculationComponents,
-                                           options: [])
-    
-    return 1.0-Float(components!.day)/Float(365)
-  }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return birthdays.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifer, for: indexPath as IndexPath) as! BirthdayCell
+
+        let birthday = birthdays[indexPath.row]
+        cell.updateWithItem(item: birthday, progress: progressUntilBirthday(birthday: birthday))
+
+        return cell
+    }
+
+    func progressUntilBirthday(birthday: Birthday) -> Float {
+
+        guard let todayComponents = todayComponents else { return 0.0 }
+
+        let calculationComponents = birthday.birthday.copy() as! NSDateComponents
+        calculationComponents.year = todayComponents.year
+
+        if calculationComponents.month < todayComponents.month ||
+            (calculationComponents.month == todayComponents.month &&
+                calculationComponents.day < todayComponents.day) {
+
+            calculationComponents.year += 1 // Swift 3 compliant ...
+        }
+
+        let components = gregorian?.components([.day],
+                                               from: todayComponents as DateComponents,
+                                               to: calculationComponents as DateComponents,
+                                               options: [])
+
+        return 1.0-Float(components!.day!)/Float(365)
+    }
 }
 
 extension BirthdaysListDataProvider {
-  func addBirthday(birthday: Birthday) {
-    birthdays.append(birthday)
-    birthdays.sortInPlace { progressUntilBirthday($0) > progressUntilBirthday($1) }
-  }
+    func addBirthday(birthday: Birthday) {
+        birthdays.append(birthday)
+        birthdays.sort { progressUntilBirthday(birthday: $0) > progressUntilBirthday(birthday: $1) }
+    }
 }
