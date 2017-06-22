be3ffdbf004da64320ca4cbe6f3107cde78a67f2
Jess <jessachandler@gmail.com>
2017-06-23 08:51:15 +0900

    Enable function in Swift 3.1
    update view controller to work in Swift 3.1
    Add picker predicate to limit enabled contacts to those with birthday

--- Birthdays/BirthdaysList/BirthdaysListViewController.swift
+++ Birthdays/BirthdaysList/BirthdaysListViewController.swift
@@ -10,39 +10,61 @@ import UIKit
 import ContactsUI
 
 class BirthdaysListViewController: UITableViewController {
-  
-  var dataProvider: BirthdaysListDataProvider?
-  
-  override func viewDidLoad() {
-    
-    tableView.dataSource = dataProvider
-    
-    dataProvider?.registerCellsForTableView(tableView)
-    
-    let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "addPerson")
-    navigationItem.rightBarButtonItem = addButton
-  }
-  
-  override func viewWillAppear(animated: Bool) {
-    super.viewWillAppear(animated)
-    dataProvider?.today = NSDate()
-  }
-  
-  func addPerson() {
-    let picker = CNContactPickerViewController()
-    picker.delegate = self
-    presentViewController(picker, animated: true, completion: nil)
-  }
+
+    // MARK: Properties
+    var dataProvider: BirthdaysListDataProvider?
+
+    // MARK: LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        view.backgroundColor = UIColor.red
+
+        // Tell self to get data from the data provider
+        tableView.dataSource = dataProvider
+        dataProvider?.registerCellsForTableView(tableView: tableView)
+
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(BirthdaysListViewController.addPerson))
+        navigationItem.rightBarButtonItem = addButton
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        dataProvider?.today = NSDate()
+    }
+
+    func addPerson() {
+        let picker = CNContactPickerViewController()
+        picker.delegate = self
+        picker.predicateForEnablingContact = NSPredicate(format: "birthday.@count > 0")
+        present(picker, animated: true, completion: nil)
+    }
 }
 
 extension BirthdaysListViewController: CNContactPickerDelegate {
-  func contactPicker(picker: CNContactPickerViewController, didSelectContact contact: CNContact) {
-  
-    if let birthday = contact.birthday {
-      let person = Birthday(firstName: contact.givenName, lastName: contact.familyName, birthday: birthday)
-      print(person)
-      dataProvider?.addBirthday(person)
-      tableView.reloadData()
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact){
+
+//        don't need if let because used predicate to only enable with birthday!
+//        if let birthday = contact.birthday {
+            let person = Birthday(firstName: contact.givenName, lastName: contact.familyName, birthday: contact.birthday! as NSDateComponents)
+            print(person)
+            dataProvider?.addBirthday(birthday: person)
+            tableView.reloadData()
+//        }
     }
-  }
+    func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
+        print("Cancel Contact Picker")
+    }
 }
+
+
+
+
+
+
